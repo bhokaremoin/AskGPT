f1688a7ade0e949673b86715abe82309510a7d2d
bhokaremoin <75439850+bhokaremoin@users.noreply.github.com>
2023-04-15 23:20:41 +0530

    getAudio.js and getTranscribe.js added and transcribe of test video generated and stored in transcribe.txt

---
+++ transcribe/getTranscribe.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const { Configuration, OpenAIApi } = require("openai");
+const fs = require("fs");
+async function transcribe() {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  const resp = await openai.createTranscription(
+    fs.createReadStream("audio.mp3"),
+    "whisper-1"
+  );
+  console.log(resp.data.text);
+  return resp.data.text;
+}
+async function saveFile() {
+  const jsonContent = await transcribe();
+  const jsonString = JSON.stringify(jsonContent);
+  fs.writeFile("transcribe.txt", jsonString, function (err) {
+    if (err) {
+      console.log("An error occured while writing JSON Object to File.");
+      return console.log(err);
+    }
+    console.log("JSON file has been saved.");
+  });
+}
+
+saveFile();
