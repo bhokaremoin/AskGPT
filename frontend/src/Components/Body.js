4689ac9574aae94a2a5ca37207da2be85e8f7749
bhokaremoin <75439850+bhokaremoin@users.noreply.github.com>
2023-04-21 22:19:00 +0530

    added node version to solve openai api error

--- frontend/src/Components/Body.js
+++ frontend/src/Components/Body.js
@@ -50,13 +50,10 @@ const Body = () => {
                   Ask AI your Question Related to the following Youtube Video
                 </h3>
                 <iframe
+                  title="you"
                   className="youtube-video col-sm"
-                  height="200"
-                  src="https://www.youtube.com/embed/oL1uem6-3m4?controls=0"
-                  title="YouTube video player"
-                  frameborder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  allowfullscreen
+                  height={200}
+                  src="https://www.youtube.com/embed/oL1uem6-3m4"
                 ></iframe>
               </div>
               <h4>Examples</h4>
@@ -83,7 +80,6 @@ const Body = () => {
           id="outlined-textarea"
           label="Ask Question"
           placeholder="Ask Question Here"
-          multiline
           value={question}
           onChange={handleChange}
         />
