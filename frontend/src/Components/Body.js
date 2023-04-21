cc051eb0d6bef556d8cace57f265c059c60cc8eb
bhokaremoin <75439850+bhokaremoin@users.noreply.github.com>
2023-04-21 19:31:35 +0530

    deploy site changes

--- frontend/src/Components/Body.js
+++ frontend/src/Components/Body.js
@@ -11,13 +11,16 @@ const Body = () => {
     setLoading(true);
     // const api_url = "http://localhost:5000/api/getAnswer";
     const api_url = "https://askgpt-server.onrender.com/api/getAnswer";
-    const response = await fetch(api_url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ question: question }),
-    });
+    const response = await fetch(
+      "https://askgpt-server.onrender.com/api/getAnswer",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ question: question }),
+      }
+    );
     const data = await response.json();
     if (!data.success) {
       alert("Something Went Wrong!\nTry Again");
